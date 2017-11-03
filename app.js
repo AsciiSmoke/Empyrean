8c4d82dd8755c412f4a492f6cd44a419271a013c
Craig Poole <craig.poole@hitched.email>
2017-11-03 11:04:58 +0000

    fixed routing to home page so you don't need to specify '/index.htm'

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.static("./"));
 
 app.get("/", function (req, res) {
-    res.send("Hello World!");
+    res.sendFile(__dirname + "/index.htm");
 });
 
 app.post("/", function (req, res) {
