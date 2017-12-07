ad9ee6670e87b0a4389b697d5baa1ef9f6d4773f
Craig Poole <craig.poole@hitched.email>
2017-12-07 17:44:20 +0000

    ...

--- src/scripts/components/card/card.tsx
+++ src/scripts/components/card/card.tsx
@@ -4,6 +4,6 @@ import { TestApi } from "../../models/testapi";
 export class Card extends React.Component<any, {}> {
     render() {
         console.log("I am", this.props.item.name);
-        return <div className="card"><img src={this.props.item.imageUrl} /></div>;
+        return <img className="card" src={this.props.item.imageUrl} />;
     }
 }
